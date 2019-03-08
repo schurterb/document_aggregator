64bb391cc8f87e6406dc5b587bca74898dc905ac
EC2 Default User <ec2-user@ip-172-16-80-48.ec2.internal>
2019-03-08 21:52:01 +0000

    Fri Mar  8 21:52:01 UTC 2019

---
+++ build.py
@@ -0,0 +1,30 @@
+
+# coding: utf-8
+
+# This notebook is for creating the python script necessary to build the documentation aggregator project
+
+# In[1]:
+
+
+lambda_client = boto3.client('lambda')
+
+filebase = "deploy/"
+filename = "scraper/test.py"
+project_bucket = "documentation-aggregator"
+
+data = dict()
+data["bucket"] = project_bucket
+data["key"] = filebase+filename
+with open(data["key"], 'rb') as f:
+    data["body"] = f.read()
+
+    
+def invokeLambdaFunction(functionName, eventData):
+    invoke_response = lambda_client.invoke(FunctionName=functionName,
+                                       LogType='Tail',
+                                       Payload=eventData,
+                                       InvocationType='RequestResponse')
+    return invoke_response
+
+print(invokeLambdaFunction("storeObjectInS3", data))
+
