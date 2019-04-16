dee84078c5f9b4a9e840f259e3d4af2b1d3fb491
Ubuntu <ubuntu@ip-172-31-60-137.ec2.internal>
2019-04-16 20:21:33 +0000

    Testing AWS lambda virtual environment handling.

--- setup_virtual_env.sh
+++ setup_virtual_env.sh
@@ -3,12 +3,20 @@
 project_name=$1
 
 rm -fr $project_name
-pip3 install --upgrade virtualenv
+pip install --upgrade virtualenv
 virtualenv -p python3 $project_name
 #python3 -m venv $project_name
 source $project_name/bin/activate
 
 pip install -r build-requirements.txt
 pip install -r lambda-requirements.txt
+
+echo ""
+echo " *** "
+echo ""
+ls document_aggregator/lib/python3.6/site-packages/
+echo ""
+echo " *** "
+echo ""
 
 deactivate
