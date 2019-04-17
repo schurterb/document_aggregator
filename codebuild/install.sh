e4ce2243771239fdf57528e43cc3d0e38931a89c
Ubuntu <ubuntu@ip-172-31-60-137.ec2.internal>
2019-04-17 15:11:16 +0000

    Broke codebuild steps out into separate scripts to work around various codebuild quirks.

--- codebuild/install.sh
+++ codebuild/install.sh
@@ -2,7 +2,4 @@
 
 #To simplify dealing with codebuild and codepipeline quirks, this script is executed in the install segment of code build.
 
-project_name=document_aggregator
-export project_name
-
 . setup_virtual_env.sh $project_name
