08c260c7d357b6c2a70df779100a41f3d7816234
Ubuntu <ubuntu@ip-172-31-60-137.ec2.internal>
2019-04-17 18:51:47 +0000

    Removed test scripts.  Added cloudformation yaml files for deploying the web and scraper sides of this project.

--- test.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-#This script is for testing various python methods necessary for the document aggregator
-
-print("Begin test script")
-
-print("End test script")
