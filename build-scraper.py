1e2bd4851e4e783512d7611c1890cbe76af6c92a
Ubuntu <ubuntu@ip-172-31-60-137.ec2.internal>
2019-04-22 16:06:42 +0000

    Removed attempts to add headless chrome lambda layer in scripts.  Added said layer manually because of its uniqueness and size.

--- build-scraper.py
+++ build-scraper.py
@@ -8,43 +8,39 @@ from sh import mkdir, cp, rm
 from buildtools import updateLambdaFunction, invokeLambdaFunction, createLambdaFunction, createOrUpdateLambdaLayer
 
 resourceOriginalDirectory = "document_aggregator/lib/python3.6/site-packages/"
-resourceZipDirectory = "python/"
-chromeBinaryDirectory = "bin/"
 
 lambdaLayers = {}
+resourceZipDirectories = {}
 
 #Create a lambda layer for boto3
-lambdaLayers['boto3'] = ["boto3"]
+lambdaLayers['boto3'] = [resourceOriginalDirectory+"boto3"]
+resourceZipDirectories['boto3'] = "python/"
 
 #Create a lambda layer for web scraping
-lambdaLayers['scraping'] = ["bs4", "selenium"]
+lambdaLayers['scraping'] = [resourceOriginalDirectory+"bs4", resourceOriginalDirectory+"selenium"]
+resourceZipDirectories['scraping'] = "python/"
 
 #Create a place to put the resources while zipping them.
-mkdir("-p", resourceZipDirectory)
 
 layerPublishResponse = []
 for name, sources in lambdaLayers.items():
     print("Creating lambda layer for "+name+" with "+str(sources))
+    resourceZipDirectory = resourceZipDirectories[name]
+    mkdir("-p", resourceZipDirectory)
     for source in sources:
-        cp("-rf", resourceOriginalDirectory+source, resourceZipDirectory)
+        cp("-rf", source, resourceZipDirectory)
     with ZipFile(name+".zip", 'w') as ziph:
         for root, dirs, files in os.walk(resourceZipDirectory):
             for file in files:
                 ziph.write(os.path.join(root, file))
-        #If this is the scraping layer, add bin/
-        if name is 'scraping':
-            for root, dirs, files in os.walk(chromeBinaryDirectory):
-                for file in files:
-                    ziph.write(os.path.join(root, file))
                 
     response = createOrUpdateLambdaLayer(name, name+".zip", runtimes=['python3.6','python3.7'])
     layerPublishResponse.append(response)
     for source in sources:
-        rm("-rf", resourceZipDirectory+source)
+        rm("-rf", source)
     print(response)
     print("Finished creating lambda layer")
-
-rm("-rf", resourceZipDirectory)
+    rm("-rf", resourceZipDirectory)
 
 #Create a lambda function
 lambdaFunctions = {}
