372d143bcc2c478f28725c54d1889cf0e129ed46
Ubuntu <ubuntu@ip-172-31-60-137.ec2.internal>
2019-05-03 22:35:28 +0000

    Added ResearchTopic lambda method that utilizes the other lambda functions to collect data

--- build-scraper.py
+++ build-scraper.py
@@ -11,6 +11,7 @@ filesToInject = {}
 filesToInject["GetHTMLFromURL"] = "scraper/lambda_get_html.py"
 filesToInject["QueryTopic"] = "scraper/lambda_websearch.py"
 filesToInject["ExtractText"] = "scraper/lambda_extract_text.py"
+filesToInject["ResearchTopic"] = "scraper/lambda_research_topic.py"
 yamlFile = "scraper/scraper_deploy.yml"
 
 print("Injecting python code into lambda files")
---
+++ scraper/lambda_research_topic.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import json
+import os
+os.environ["PATH"] += os.pathsep + "/opt/python"
+
+import boto3
+
+def lambda_handler(event, context):
+    
+    #Get topic string from event data
+    topic = event['topic']
+    print("Researching topic: "+topic)
+    
+    #Get links by calling lambda_websearch synchronously
+    eventData = dict(query=topic, numberOfLinks=10)
+    links = invokeLambdaFunction("QueryTopic", eventData)
+    print("Received "+str(len(links))+" related to topic")
+    
+    #Get text data by calling lambda_extract_text asynchronously
+    # passing a different URL to each instance
+    for link in links:
+        print("Extracting text from "+link)
+        eventData = dict(url=link)
+        invokeLambdaFunction("ExtractText", eventData, asynchronous=True)
+    
+        
+"""
+@param functionName  name of the lambda function to invoke
+@param eventData     data to pass to the lambda function
+"""
+def invokeLambdaFunction(functionName, eventData, asynchronous=False):
+    lambda_client = boto3.client('lambda')
+    if asynchronous:
+        invoke_response = lambda_client.invoke(FunctionName=functionName,
+                                           Payload=json.dumps(eventData),
+                                           InvocationType='Event')
+    else:
+        invoke_response = lambda_client.invoke(FunctionName=functionName,
+                                           LogType='Tail',
+                                           Payload=json.dumps(eventData),
+                                           InvocationType='RequestResponse')
+        return invoke_response
+        
