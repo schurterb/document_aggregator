eb0058926b9f4b0cbdddeed009ad06c8dc76efd0
Ubuntu <ubuntu@ip-172-31-60-137.ec2.internal>
2019-07-23 15:51:59 +0000

    Testing how to create index.html without newline characters appearing in the web page

--- build-webpage.py
+++ build-webpage.py
@@ -25,6 +25,7 @@ print("Storing "+sourceFile+" in s3...")
 
 with open(sourceFile, 'r') as f:
     body_data = f.read()
+    print(body_data)
 
 data = { "bucket": project_bucket, "key": destinationFile, "body": body_data, "content_type": "text/html" }
 #Cannot have ACL set to public for static webpage
